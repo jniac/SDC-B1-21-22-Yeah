aede9871c1aa5fd116eb5140ca8332335543b7e4
UtopisteNjoly <nicolas.joly@supdecreation.com>
2022-03-16 11:59:58 +0100

    Big first Update

--- Assets/People/UtopisteNJoly/Scripts/UtopisteNjoly_HardJumper.cs
+++ Assets/People/UtopisteNJoly/Scripts/UtopisteNjoly_HardJumper.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UtopisteNjoly_HardBumper : MonoBehaviour
+public class UtopisteNjoly_HardJumper : MonoBehaviour
 {
     public float jumpForce = 15f;
 
@@ -10,6 +10,7 @@ public class UtopisteNjoly_HardBumper : MonoBehaviour
     {
         // Debug.Break();
         Rigidbody body = collider.attachedRigidbody;
+
         if (body != null)
         {
             body.velocity = new Vector3(0f, jumpForce, 0f);
