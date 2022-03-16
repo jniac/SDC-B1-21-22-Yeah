3c372aaa72e8383294fbc3593ac6d6b2a2d22483
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-03-16 13:34:28 +0100

    jniac update (scene, prefab, etc.)

--- Assets/People/jniac/Scripts/jniac_TriggerClone.cs
+++ Assets/People/jniac/Scripts/jniac_TriggerClone.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class jniac_TriggerClone : MonoBehaviour
 {
+    public LayerMask mask = ~0;
     public float cooldownDuration = 0.1f;
 
     float cloneTime = 0;
 
+    bool Match(int layer) => (mask & (1 << layer)) != 0;
+
     void OnTriggerEnter(Collider other)
     {
         var source = other.attachedRigidbody.gameObject;
-        var cooling = Time.time - cloneTime < cooldownDuration;
-        if (cooling == false)
-        {
-            cloneTime = Time.time;
-            Instantiate(source);
+
+        if (Match(source.layer)) {
+
+            var cooling = Time.time - cloneTime < cooldownDuration;
+            
+            if (cooling == false)
+            {
+                cloneTime = Time.time;
+                Instantiate(source);
+            }
         }
     }
 }
