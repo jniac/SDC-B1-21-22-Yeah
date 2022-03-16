70646e581eb7e32eaccb6fa9eb501eed638a1505
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-03-16 13:20:08 +0100

    phew hard-to-code "auto-ground" feature

--- Assets/Common/Scripts/StaticItemSnapping.cs
+++ Assets/Common/Scripts/StaticItemSnapping.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,42 @@ using UnityEngine;
 [ExecuteAlways]
 public class StaticItemSnapping : MonoBehaviour
 {
+    public LayerMask groundMask = 1;
+    public float yOver = 0f;
+
+    float GetY(float defaultY)
+    {
+        var hits = Physics.RaycastAll(transform.position + Vector3.up * 10f, Vector3.down, 100f, groundMask);
+
+        float y = float.NegativeInfinity;
+        foreach (var hit in hits)
+        {
+            if (hit.collider.gameObject == gameObject)
+                continue;
+
+            float currentY = hit.point.y + yOver;
+            if (currentY > y)
+                y = currentY;
+        }
+
+        // Ignore invalid y value.
+        if (y == float.NegativeInfinity)
+            return defaultY;
+        
+        return y;
+    }
+
     void Snap()
     {
         Vector3 position = transform.position;
         position += -Vector3.one / 2f;
         position.x = Mathf.Round(position.x);
         position.z = Mathf.Round(position.z);
-        transform.position = position + Vector3.one / 2f;
+        position += Vector3.one / 2f;
+        
+        position.y = GetY(position.y);
+
+        transform.position = position;
     }
 
     void Update()
