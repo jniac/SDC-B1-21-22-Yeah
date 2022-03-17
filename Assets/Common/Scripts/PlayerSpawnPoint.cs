1cd674cdcdcf2cb1ade349c006a3a8e369978321
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-03-17 14:35:19 +0100

    "TP Player" (PlayerSpawnPoint utility)

--- Assets/Common/Scripts/PlayerSpawnPoint.cs
+++ Assets/Common/Scripts/PlayerSpawnPoint.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class PlayerSpawnPoint : MonoBehaviour
 {
@@ -20,4 +23,22 @@ public class PlayerSpawnPoint : MonoBehaviour
             activationTime = Time.time;
         }
     }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(PlayerSpawnPoint))]
+    class MyEditor : Editor {
+        PlayerSpawnPoint Target => target as PlayerSpawnPoint;
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("TP Player"))
+            {
+                var player = GameObject.FindGameObjectWithTag("Player");
+                if (player != null)
+                    player.transform.position = Target.transform.position;
+            }
+        }
+    }
+#endif
 }
