723255b477eb9c5fb0fdbdf6a161530eb0625c5f
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-03-28 12:23:14 +0200

    Destroyer fix: invicibility + trigger enter bug

--- Assets/Common/Scripts/Gameplay/Destroyer.cs
+++ Assets/Common/Scripts/Gameplay/Destroyer.cs
@@ -40,7 +40,7 @@ public class Destroyer : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if (destroyCondition == DestroyCondition.Trigger || destroyCondition == DestroyCondition.Both)
         {
