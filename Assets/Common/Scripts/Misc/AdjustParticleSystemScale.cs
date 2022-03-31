e52ca95a52db5c329571626074277af423243708
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-03-31 23:00:13 +0200

    better

--- Assets/Common/Scripts/Misc/AdjustParticleSystemScale.cs
+++ Assets/Common/Scripts/Misc/AdjustParticleSystemScale.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class AdjustParticleSystemScale : MonoBehaviour
 {
+    public float rateByUnitSquare = 15f;
+
     #if UNITY_EDITOR
     void Update()
     {
         if (Application.isPlaying == false)
         {
             var system = GetComponent<ParticleSystem>();
+
             var shape = system.shape;
             shape.scale = transform.parent.localScale;
+
+            var x = transform.parent.localScale.x;
+            var z = transform.parent.localScale.x;
+            var emission = system.emission;
+            emission.rateOverTime = x * z * rateByUnitSquare;
         }
     }
     #endif
