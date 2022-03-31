ea76d015c778d50e4e6f876cd785950524b9b97a
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-03-31 17:14:57 +0200

    When it's better, it's better

--- Assets/People/jniac/Scripts/jnc_Coin.cs
+++ Assets/People/jniac/Scripts/jnc_Coin.cs
@@ -24,21 +24,11 @@ public class jnc_Coin : MonoBehaviour
 
     public CoinType type = CoinType.Normal;
 
-    public GameObject[] particles;
-
-    public int particleCount = 10;
-
-    void Collect()
-    {
-        Destroy(gameObject);
-        Boom.FromPoint(transform.position, particles, particleCount);
-    }
-
     void OnTriggerEnter(Collider other)
     {
         // The player only has the ability to collect coins.
         if (other.attachedRigidbody.gameObject.tag == "Player")
-            Collect();
+            Destroy(gameObject);
     }
 
 #if UNITY_EDITOR
